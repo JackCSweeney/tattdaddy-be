d3f59351d54c2019bf2e09f218d4cc4779c18292
Joey Reyes <joeyreyes105@gmail.com>
2024-04-10 15:17:24 -0700

    Create test for deleting a tattoo user relation

--- spec/requests/api/v0/user_tattoos/delete_user_tattoos_request_spec.rb
+++ spec/requests/api/v0/user_tattoos/delete_user_tattoos_request_spec.rb
@@ -20,6 +20,8 @@ RSpec.describe "endpoint delete /api/v0/user_tattoos" do
         "CONTENT_TYPE": "application/json",
         "ACCEPT": "application/json"
       }
+
+      @user_tattoo = UserTattoo.create!(@params)
     end
 
     it "deletes a user tattoo relation via HTTP request" do
@@ -27,6 +29,24 @@ RSpec.describe "endpoint delete /api/v0/user_tattoos" do
 
         expect(response).to be_successful
         expect(response.status).to eq(204)
+    end
+
+    describe "Sad Path" do 
+      it "errors if association does not exist" do 
+        delete "/api/v0/user_tattoos", params: JSON.generate(user_tattoo: @params), headers: @headers
+
+        delete "/api/v0/user_tattoos", params: JSON.generate(user_tattoo: @params), headers: @headers
+        expect(response).not_to be_successful
+        expect(response.status).to eq(404)
+
+        error_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(error_response).to have_key(:errors)
+        expect(error_response[:errors]).to be_a(Array)
+
+        expect(error_response[:errors].first).to have_key(:detail)
+        expect(error_response[:errors].first[:detail]).to eq("Association between Tattoo and User does not exist")
+      end
     end
   end 
 end
