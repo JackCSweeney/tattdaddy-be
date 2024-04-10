b920274b3b94f2c0ff2c18ed43d99d06a0790e6e
Joey Reyes <joeyreyes105@gmail.com>
2024-04-09 18:20:29 -0700

    Create endpoint for creating an artist, happy and sad path tests pass

--- app/controllers/api/v0/artists_controller.rb
+++ app/controllers/api/v0/artists_controller.rb
@@ -3,4 +3,15 @@ class Api::V0::ArtistsController < ApplicationController
     artist = Artist.find(params[:id])
     render json: ArtistSerializer.new(artist)
   end
+
+  def create
+    artist = Artist.create!(artist_params)
+    render json: ArtistSerializer.new(artist), status: :created
+  end
+
+  private 
+
+  def artist_params
+    params.require(:artist).permit(:name, :location, :email, :password)
+  end
 end
