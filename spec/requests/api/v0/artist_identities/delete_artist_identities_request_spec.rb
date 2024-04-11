21dd19bacd46d5bae40c18d529eefd580f953185
Joey Reyes <joeyreyes105@gmail.com>
2024-04-10 18:37:21 -0700

    Create happy path test for delete action

--- spec/requests/api/v0/artist_identities/delete_artist_identities_request_spec.rb
+++ spec/requests/api/v0/artist_identities/delete_artist_identities_request_spec.rb
@@ -1,13 +1,30 @@
 require "rails_helper"
 
-RSpec.describe "endpoint ", type: :feature do
-  describe "As a visitor" do
+RSpec.describe "endpoint delete /api/v0/artist_identities" do
+  describe "As a User" do
 
-  before do
-      url_path
-  end
+    before do
+      @artist = create(:artist)
+      @identity = Identity.create!({identity_label: "Female"})
 
-  it "" do
-      
-  end
+      @params = {
+        artist_id: @artist.id,
+        identity_id: @identity.id
+      }
+
+      @headers = {
+        "CONTENT_TYPE": "application/json",
+        "ACCEPT": "application/json"
+      }
+
+      @artist_identity = ArtistIdentity.create!(@params)
+    end
+
+    it "deletes a artist identity relation via HTTP request" do
+      delete "/api/v0/user_tattoos", params: JSON.generate(artist_identity: @params), headers: @headers
+
+      expect(response).to be_successful
+      expect(response.status).to eq(204)
+    end
+  end 
 end
