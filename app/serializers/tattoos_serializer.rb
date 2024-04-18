01defc5cf8fabad6dc51f8092217594b1e7e7f4d
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-04-18 14:44:45 -0600

    artist req scheduling_link

--- app/serializers/tattoos_serializer.rb
+++ app/serializers/tattoos_serializer.rb
@@ -1,4 +1,12 @@
 class TattoosSerializer
   include JSONAPI::Serializer
   attributes :image_url, :price, :time_estimate, :artist_id
+
+  belongs_to :artist do
+    data do
+      {
+        scheduling_link: @object.artist.scheduling_link
+      }
+    end
+  end
 end
--- spec/factories/artists.rb
+++ spec/factories/artists.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     name { Faker::Name.name }
     email { Faker::Internet.email }
     location { Faker::Address.full_address }
+    scheduling_link { Faker::Internet.url }
     password { "FakePassword1!" }
   end
 end
